dentproject/meta-dentos

commit 78b3cbf705323dc4a1445a448a2e1db64347d122

recipes-network: add mstpd package recipe
Signed-off-by: Jakov Petrina <jakov.petrina@sartura.hr>

recipes-network/mstpd/mstpd_git.bb
@@ -1,0 +1,33 @@
+SUMMARY = "Multiple Spanning Tree Protocol Daemon"
+DESCRIPTION = "${SUMMARY}"
+LICENSE = "GPL-2.0-only"
+LIC_FILES_CHKSUM = "file://LICENSE;md5=4325afd396febcb659c36b49533135d4"
+
+SRCBRANCH = "master"
+SRCREV = "20f1c93ec1b0fb9056c1770deadb88f57fb71024"
+
+SRC_URI = " \
+  git://github.com/mstpd/mstpd.git;branch=${SRCBRANCH};protocol=https \
+"
+
+S = "${WORKDIR}/git"
+
+inherit autotools systemd
+
+EXTRA_OECONF = "--sbindir=/sbin"
+
+CFLAGS += " -DPISM_ENABLE_LOG"
+CFLAGS += " -DPRTSM_ENABLE_LOG"
+CFLAGS += " -DPACKET_DEBUG"
+
+FILES:${PN} += "${systemd_system_unitdir}/mstpd.service"
+
+SYSTEMD_SERVICE:${PN} = "mstpd.service"
+SYSTEMD_AUTO_ENABLE:${PN} = "disable"
+
+do_install:append() {
+   rm ${D}${libexecdir}/mstpctl-utils/ifquery
+
+   install -d ${D}${systemd_unitdir}/system
+   install -m 0644 ${WORKDIR}/build/utils/mstpd.service ${D}${systemd_unitdir}/system
+}

commit 9b22e4ea663581a0ad89d49b6806207e3e163a9f

mstpd: do not set sbindir to /sbin
Do not set sbindir to /sbin, as /sbin is not a valid location when
usrmerge is enabled. Yocto will automatically pass the correct argument,
so there is no need to set it at all, therefore drop it.

Fixes the following build error:

ERROR: mstpd-git-r0 do_package: QA Issue: mstpd: Files/directories were installed but not shipped in any package:
  /sbin/bridge-stp
  /sbin/mstpctl
  /sbin/mstp_restart
  /sbin/mstpd
Please set FILES such that these items are packaged. Alternatively if they are unneeded, avoid installing them or delete them within do_install.
mstpd: 4 installed and not shipped files. [installed-vs-shipped]

Fixes: 78b3cbf70532 ("recipes-network: add mstpd package recipe")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

recipes-network/mstpd/mstpd_git.bb
@@ -14,8 +14,6 @@ S = "${WORKDIR}/git"
 
 inherit autotools systemd
 
-EXTRA_OECONF = "--sbindir=/sbin"
-
 CFLAGS += " -DPISM_ENABLE_LOG"
 CFLAGS += " -DPRTSM_ENABLE_LOG"
 CFLAGS += " -DPACKET_DEBUG"